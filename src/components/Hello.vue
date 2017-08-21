a91c04392b3b965d876c0396ae3d91e60d192cf5
Nick Ford <nickforddesign@gmail.com>
2017-08-21 11:52:23 -0400

    initial commit, got brace editor working with handlebars syntax

---
+++ src/components/Hello.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hello">
+    <div class="editor">
+      <editor></editor>
+    </div>
+    <!-- <editor v-model="content" @init="editorInit()" lang="html" theme="chrome" width="500" height="100"></editor> -->
+  </div>
+</template>
+
+<script>
+import editor from './editor'
+export default {
+  name: 'hello',
+  data () {
+    return {
+    }
+  },
+  components: {
+    editor
+    // editor: require('vue2-ace-editor')
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .editor {
+    width: 50%;
+  }
+</style>
