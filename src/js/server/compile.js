5c2a12c05a36c4665f325f9b8af01e8ed31e0b3f
Nicholas Ford <nickforddesign@gmail.com>
2020-08-18 19:14:03 -0400

    Update compile.js

--- src/js/server/compile.js
+++ src/js/server/compile.js
@@ -6,10 +6,8 @@ module.exports = async function compile(template, data, partials) {
     const args = [ data ]
     if (partials !== undefined) args.push(partials)
     const html = compiled.render(...args)
-    // return Promise.resolve(html)
     return html
   } catch (error) {
     throw error
-    // return Promise.reject(error)
   }
 }
